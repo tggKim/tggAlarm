0db0de2ef81932a9df788622cc3ac1ba60d1049b
tgg <qazplmtfgcv123@gmail.com>
2025-10-20 22:44:45 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/kim/messageService/MessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kim.messageService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
